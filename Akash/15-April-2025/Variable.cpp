1214971e4f75d16cbaecdd2620d71697467ef7e1
CrazyCoders001 <akasgkanni45@gmail.com>
2025-04-15 20:05:17 +0530

    Update Variable.cpp

--- Akash/15-April-2025/Variable.cpp
+++ Akash/15-April-2025/Variable.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+//why to use variables
+
+/*
+1. To store values
+2. To avoid multi line edit work
+3. To fit in right kind of data in right place
+*/
+
+// when you say num1=15, num1 gets 4 bytes of memory, and those 4 bytes are used to store number 15
+int main(){
+
+    int num1 = 15, num2 = 4;
+
+    cout<<num1+num2<<endl;
+
+    cout<<num1*num2<<endl;
+
+    cout<<num1-num2<<endl;
+
+
+
+    //numb 10 can be used in 1000+ lines
+}
