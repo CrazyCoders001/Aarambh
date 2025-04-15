36ff27cecb6d20061820dbd875f1b9a29bbaf075
CrazyCoders001 <akasgkanni45@gmail.com>
2025-04-15 20:05:55 +0530

    Create Data types.cpp

---
+++ Akash/15-April-2025/Data types.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   //List of all data types
+   /*
+   int, char, string, float, double, boolean
+   */
+
+  // 1 byte = 8 bites (bits = 10010001)
+  // int ka size hota hai 2 or 4 bytes, uska mtlb int certain level ke bad value accept nhi krega
+  // max int value we can store is 2147483647
+
+  int num = 2147483648;
+  cout<<num;
+
+  // int mai float wala value assign kr diya
+  // what was the impact and what that company suffered
+  // 5 Rockets mid air mai blast ho gaye
+
+  int num2 = 10.23;
+}
