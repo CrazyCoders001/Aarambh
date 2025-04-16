2efabad0d0b1380175219f47eb56cb4c6d925c80
unknown <154747233+ReallyDontknowU@users.noreply.github.com>
2025-04-16 21:07:16 +0530

    Create Data_types.cpp

---
+++ Muzaffar/15-apr-25/Data_types.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Integer types
+    int myInt = 10;
+    short myShort = 5;
+    long myLong = 123456;
+    long long myLongLong = 1234567890;
+
+    // Floating-point types
+    float myFloat = 3.14f;
+    double myDouble = 3.1415926535;
+    long double myLongDouble = 3.141592653589793;
+
+    // Character type
+    char myChar = 'A';
+
+    // Boolean type
+    bool myBool = true;
+
+    // Display all values
+    cout << "int: " << myInt << endl;
+    cout << "short: " << myShort << endl;
+    cout << "long: " << myLong << endl;
+    cout << "long long: " << myLongLong << endl;
+    cout << "float: " << myFloat << endl;
+    cout << "double: " << myDouble << endl;
+    cout << "long double: " << myLongDouble << endl;
+    cout << "char: " << myChar << endl;
+    cout << "bool: " << myBool << endl;
+
+
+    return 0;
+}
