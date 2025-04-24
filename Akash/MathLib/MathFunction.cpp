89571b36a92b0bc09167271a2002c40ffd5144d8
CrazyCoders001 <akasgkanni45@gmail.com>
2025-04-24 20:05:01 +0530

    Update MathFunction.cpp

--- Akash/MathLib/MathFunction.cpp
+++ Akash/MathLib/MathFunction.cpp
@@ -41,6 +41,39 @@ int main(){
 
 
     // round
+    cout<<"----------------------round function---------------"<<endl;
+    // round function is used to convert floating point variables to integers
+    // while doing round when the decimal point is from x.1 to x.4 (x can be any integer) 
+    // --> so the integer answer of input will be nearest smallest int value (4.3 -> 4)
+
+    // while doing round when the decimal point is from x.5 to x.9 (x can be any integer) 
+    // --> so the integer answer of input will be nearest greater int value (4.7 -> 5)
+
+    int lowerRound = round(4.3);
+    int higherRound = round(4.7);
+
+    cout<<"lowerRound of 4.3 is :- "<<lowerRound<<endl;
+    cout<<"higherRound of 4.7 is :- "<<higherRound<<endl;
+
     // pow
+    cout<<"----------------------pow function---------------"<<endl;
+    // power function is used to get the power value and the syntax will look like this
+    // int ans =  pow(base, exponent)
+
+    int base = 5;
+    int Exponent = 3;
+    int powerValue = pow(base, Exponent);   //5*5*5 = 
+
+    cout<<"The powerValue for pow(5,3) is :- "<<powerValue<<endl;
+
     // abs
+    cout<<"----------------------abs function---------------"<<endl;
+    // int absValue = abs(input);
+    // the absolute function give the absolute value (It removes the sign for example the negative sign)
+    input = -10;
+    int inp1 = 20;
+
+
+    cout<<"The absolute value of -10 : "<<abs(input)<<endl;
+    cout<<"The absolute value of 20 : "<<abs(inp1)<<endl;
 }
