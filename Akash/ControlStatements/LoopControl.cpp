9c2db37a9f0e09f9d8eab19a367f118879a5783c
CrazyCoders001 <akasgkanni45@gmail.com>
2025-04-28 20:34:33 +0530

    Create LoopControl.cpp

---
+++ Akash/ControlStatements/LoopControl.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // 2. Loop Control   :- for, while, do while
+
+    // Why we should use loops? :- To avoid repetation of code
+                                 //To write maintainable code
+                                 //To write readable code
+    
+
+    cout<<"--------------------For loop---------------------"<<endl;
+    // for :- it is used to perform iteration 
+    // syntax :- for(initialization; condition; increment/decrement)
+
+    // 10 bar hello world print karo
+    for(int i=1; i <= 10; i--)           //jb condition false hoga code flow for ke bahar jayega
+    {
+        cout<<"Hello world for i = "<<i<<endl;
+         // i = i + 2;
+    }
+
+    /*
+        i=1       ---> i--
+        i=0       ---> i--
+        i=-1      ---> i--
+
+        i= negative infinity
+
+        i will always be less than 10   -- ctrl + c
+
+    */
+
+    cout<<"For ke bahar"<<endl;
+
+    //1, 3, 5, 7, 9, 11
+
+   /*
+   
+   i=1      ---- 1<=10? yes   print   i++   , i+=2  1+=2 = 3
+   i=2      ---- 2<=10? yes   print   i++                  
+   i=3
+   i=4
+   i=5
+   i=6
+   i=7
+   i=8
+   i=9
+   i=10   ---- 10<=10? yes  print i++
+   i=11        11<=10? false
+   */
+
+
+  //while
+
+  //do while
+
+
+  //switch
+
+  // word problem ko, if else vagaire use karo aur solve karo, and usko github pe submit karo
+  // submission, under your folder, aaj ka date and prbm_1.cpp
+    
+}
